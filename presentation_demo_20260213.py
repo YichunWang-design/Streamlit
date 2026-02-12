918dedf596e15e6bea9574d571c2c3359012e7b9
YichunWang-design <chu_wang@worksid.co.jp>
2026-02-12 11:46:10 +0900

    Update dataframe display parameters
    Removed 'use_container_width' parameter from dataframe display.

--- presentation_demo_20260213.py
+++ presentation_demo_20260213.py
@@ -331,7 +331,7 @@ def main():
         display_df = final_filtered_df[["オーダー日", "カテゴリ", "サブカテゴリ", "商品", "売上"]].copy()
         styled_df = display_df.style.background_gradient(cmap='Blues', subset=['売上']).format({'売上': '{:,.0f} 円'})
         
-        st.dataframe(styled_df, hide_index=True, height=600, use_container_width=True)
+        st.dataframe(styled_df, hide_index=True, height=600)
         
         # 下部の余白
         st.html("<div style='height:1000px;'></div>")
