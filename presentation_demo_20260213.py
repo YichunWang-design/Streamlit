b1e2486ceb76f16369f91fe756245a57e0bd145f
YichunWang-design <chu_wang@worksid.co.jp>
2026-02-12 12:01:53 +0900

    Update
    Refactor dataframe display with column configuration for sales.

--- presentation_demo_20260213.py
+++ presentation_demo_20260213.py
@@ -329,9 +329,16 @@ def main():
 
         st.subheader("📋 詳細表")
         display_df = final_filtered_df[["オーダー日", "カテゴリ", "サブカテゴリ", "商品", "売上"]].copy()
-        # styled_df = display_df.style.background_gradient(cmap='Blues', subset=['売上']).format({'売上': '{:,.0f} 円'})
         
-        st.dataframe(display_df, hide_index=True, height=600)
+        st.dataframe(
+            display_df,
+            column_config={
+                "売上": st.column_config.NumberColumn("売上 (円)", format="localized")
+            },
+            hide_index=True,
+            height=600,
+            width="stretch"
+            )
         
         # 下部の余白
         st.html("<div style='height:1000px;'></div>")
